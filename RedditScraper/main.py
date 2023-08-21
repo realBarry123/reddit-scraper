48212cd8d5570f85e4c1ba50289be0f903c2b88d
realBarry123 <77864928+realBarry123@users.noreply.github.com>
2023-08-20 19:05:07 -0700

    Scrapey scrapey

--- RedditScraper/main.py
+++ RedditScraper/main.py
@@ -1,1 +1,17 @@
-print("hello world")
+from bs4 import BeautifulSoup
+import requests
+
+def get_data(url):
+    r = requests.get(url, headers = {"Content-Type":"text"})
+    return r.text
+
+soup = BeautifulSoup(get_data("https://www.reddit.com/r/copypasta/comments/15wihsx/i_saw_a_school_girls_shoulder/"), 'html.parser')
+#print(soup)
+
+data_str = ""
+
+for item in soup.find_all("div", attrs={"class": "_2mHuuvyV9doV3zwbZPtIPG"}):
+    data_str += item.get_text()
+    print("1")
+
+print(data_str)
