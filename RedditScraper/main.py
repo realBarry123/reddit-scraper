1c1dbdde668ba2b64e4f70fe01f9bccd3be3a014
realBarry123 <77864928+realBarry123@users.noreply.github.com>
2023-08-22 12:57:56 -0700

    Selenium + No Error

--- RedditScraper/main.py
+++ RedditScraper/main.py
@@ -1,17 +1,26 @@
 from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
 
 def get_data(url):
-    r = requests.get(url, headers = {"Content-Type":"text"})
-    return r.text
+    #r = requests.get(url, headers = {"Content-Type":"text"})
+    r = driver.get(url)
+    return r.page_source
 
-soup = BeautifulSoup(get_data("https://www.reddit.com/r/copypasta/comments/15wihsx/i_saw_a_school_girls_shoulder/"), 'html.parser')
-#print(soup)
+driver = webdriver.Chrome()
+driver.get("https://www.reddit.com/r/UBC/comments/15xvfde/failed/")
+html = driver.page_source
+soup = BeautifulSoup(html, features="html.parser")
+print(soup)
+
 
 data_str = ""
 
 for item in soup.find_all("div", attrs={"class": "_2mHuuvyV9doV3zwbZPtIPG"}):
     data_str += item.get_text()
+    print("1")
+
+for item in soup.find_all("div", class_="_2mHuuvyV9doV3zwbZPtIPG"):
+    data_str += item.text.strip()
     print("1")
 
 print(data_str)
